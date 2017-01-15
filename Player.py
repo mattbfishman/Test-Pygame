09066dbd28f83fcb4de7561c614a476ebd16524b
mattbfishman <mbfishman@quinnipiac.edu>
2017-01-14 23:37:25 -0500

    Finished walk player cycles

--- Player.py
+++ Player.py
@@ -1,8 +1,103 @@
 from pygame import *
 import sys, pygame
+from SpriteStripAnim import SpriteStripAnim
 
 class Player():
 	def __init__(self, x ,y):
 		# self.sprite = sprite
 		self.x = x
 		self.y = y
+
+	def playerMove(self, player, n, right, left, up, down, defaultPlayer):
+		keys = pygame.key.get_pressed()
+		if keys[K_d]:
+		    if n >= len(right):
+		        n = 0
+		    image = right[n].next()
+		    player.x +=2
+		    return image
+		if keys[K_a]:
+			if n >= len(left):
+				n = 0
+			image = left[n].next()
+			player.x -=2
+			return image
+		if keys[K_w]:
+			if(player.y > 15):
+				if n >= len(up):
+					n = 0
+				image = up[n].next()
+				player.y -=2
+				return image
+			else:
+				if n >= len(up):
+					n = 0
+				image = up[n].next()
+				player.y -=0
+				return image
+		if keys[K_s]:
+			if n >= len(down):
+				n = 0
+			image = down[n].next()
+			player.y +=2
+			return image
+		else:
+			n = 0 
+			return defaultPlayer
+
+	def rightSprite(self):
+		rightWalkSprite = [
+		    SpriteStripAnim('sprites/rightwalk.png', ((0,0,64,64)), 9, 0, True, 10),
+		    SpriteStripAnim('sprites/rightwalk.png', ((64,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/rightwalk.png', ((128,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/rightwalk.png', ((192,64,48,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/rightwalk.png', ((256,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/rightwalk.png', ((320,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/rightwalk.png', ((384,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/rightwalk.png', ((448,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/rightwalk.png', ((512,0,64,64)), 0, 0, True, 10)
+		] 
+		return rightWalkSprite
+
+	def leftSprite(self):
+		leftWalkSprite = [
+		    SpriteStripAnim('sprites/leftwalk.png', ((0,0,64,64)), 9, 0, True, 10),
+		    SpriteStripAnim('sprites/leftwalk.png', ((64,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/leftwalk.png', ((128,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/leftwalk.png', ((192,64,48,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/leftwalk.png', ((256,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/leftwalk.png', ((320,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/leftwalk.png', ((384,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/leftwalk.png', ((448,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/leftwalk.png', ((512,0,64,64)), 0, 0, True, 10)
+		] 
+		return leftWalkSprite
+
+	def upSprite(self):
+		upWalkSprite = [
+		    SpriteStripAnim('sprites/upwalk.png', ((0,0,64,64)), 9, 0, True, 10),
+		    SpriteStripAnim('sprites/upwalk.png', ((64,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/upwalk.png', ((128,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/upwalk.png', ((192,64,48,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/upwalk.png', ((256,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/upwalk.png', ((320,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/upwalk.png', ((384,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/upwalk.png', ((448,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/upwalk.png', ((512,0,64,64)), 0, 0, True, 10)
+		] 
+		return upWalkSprite
+
+	def downSprite(self):
+		downWalkSprite = [
+		    SpriteStripAnim('sprites/downwalk.png', ((0,0,64,64)), 9, 0, True, 10),
+		    SpriteStripAnim('sprites/downwalk.png', ((64,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/downwalk.png', ((128,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/downwalk.png', ((192,64,48,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/downwalk.png', ((256,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/downwalk.png', ((320,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/downwalk.png', ((384,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/downwalk.png', ((448,0,64,64)), 0, 0, True, 10),
+		    SpriteStripAnim('sprites/downwalk.png', ((512,0,64,64)), 0, 0, True, 10)
+		] 
+		return downWalkSprite
+
--- loop.py
+++ loop.py
@@ -7,42 +7,40 @@ pygame.init()
 size = width, height = 640, 480
 screen = pygame.display.set_mode(size)
 
-def anTest():
-	FPS = 120
-	frames = FPS/12
-	playerSS = spritesheet.spritesheet('sprites/rightwalk.png')
-	player = playerSS.image_at((0,0,64,64))
-	tempPlayer = Player(0,15)
-	strips = [
-	    SpriteStripAnim('sprites/rightwalk.png', ((0,0,64,64)), 9, 1, True, frames),
-	    SpriteStripAnim('sprites/rightwalk.png', ((64,0,64,64)), 0, 1, True, frames),
-	    SpriteStripAnim('sprites/rightwalk.png', ((128,0,64,64)), 0, 1, True, frames),
-	    SpriteStripAnim('sprites/rightwalk.png', ((192,64,48,64)), 0, 1, True, frames),
-	    SpriteStripAnim('sprites/rightwalk.png', ((256,0,64,64)), 0, 1, True, frames),
-	    SpriteStripAnim('sprites/rightwalk.png', ((320,0,64,64)), 0, 1, True, frames),
-	    SpriteStripAnim('sprites/rightwalk.png', ((384,0,64,64)), 0, 1, True, frames),
-	    SpriteStripAnim('sprites/rightwalk.png', ((448,0,64,64)), 0, 1, True, frames),
-	    SpriteStripAnim('sprites/rightwalk.png', ((512,0,64,64)), 0, 1, True, frames),
-	] 
-	n = 0
-	strips[n].iter()
-	image = strips[n].next()
-	while True:
-	    for e in pygame.event.get():
-	        if e.type == KEYUP:
-	            if e.key == K_ESCAPE:
-	                sys.exit()
-	        keys = pygame.key.get_pressed()
-            if keys[K_d]:
-                if n >= len(strips):
-                    n = 0
-                image = strips[n].next()
-                tempPlayer.x +=2
-            else: 
-            	image = player
-	    screen.blit(image, (tempPlayer.x,tempPlayer.y))
-	    pygame.display.flip()
-	    # clock.tick(FPS)
+# def anTest():
+# 	playerSS = spritesheet.spritesheet('sprites/rightwalk.png')
+# 	player = playerSS.image_at((0,0,64,64))
+# 	tempPlayer = Player(0,15)
+# 	strips = [
+# 	    SpriteStripAnim('sprites/rightwalk.png', ((0,0,64,64)), 9, 1, True, frames),
+# 	    SpriteStripAnim('sprites/rightwalk.png', ((64,0,64,64)), 0, 1, True, frames),
+# 	    SpriteStripAnim('sprites/rightwalk.png', ((128,0,64,64)), 0, 1, True, frames),
+# 	    SpriteStripAnim('sprites/rightwalk.png', ((192,64,48,64)), 0, 1, True, frames),
+# 	    SpriteStripAnim('sprites/rightwalk.png', ((256,0,64,64)), 0, 1, True, frames),
+# 	    SpriteStripAnim('sprites/rightwalk.png', ((320,0,64,64)), 0, 1, True, frames),
+# 	    SpriteStripAnim('sprites/rightwalk.png', ((384,0,64,64)), 0, 1, True, frames),
+# 	    SpriteStripAnim('sprites/rightwalk.png', ((448,0,64,64)), 0, 1, True, frames),
+# 	    SpriteStripAnim('sprites/rightwalk.png', ((512,0,64,64)), 0, 1, True, frames),
+# 	] 
+# 	n = 0
+# 	strips[n].iter()
+# 	image = strips[n].next()
+# 	while True:
+# 	    for e in pygame.event.get():
+# 	        if e.type == KEYUP:
+# 	            if e.key == K_ESCAPE:
+# 	                sys.exit()
+# 	        keys = pygame.key.get_pressed()
+#             if keys[K_d]:
+#                 if n >= len(strips):
+#                     n = 0
+#                 image = strips[n].next()
+#                 tempPlayer.x +=2
+#             else: 
+#             	image = player
+# 	    screen.blit(image, (tempPlayer.x,tempPlayer.y))
+# 	    pygame.display.flip()
+# 	    # clock.tick(FPS)
 
 
 def mainGame():
@@ -51,44 +49,24 @@ def mainGame():
 
 	player = Player(0,15)
 	playerRight = []
-	playerSS = spritesheet.spritesheet('sprites/rightwalk.png')
+	playerSS = spritesheet.spritesheet('sprites/downwalk.png')
 
-	FPS = 120
-	frames = FPS/12
-	playerSS = spritesheet.spritesheet('sprites/rightwalk.png')
 	defaultPlayer = playerSS.image_at((0,0,64,64), colorkey=(0,0,0))
 	tempPlayer = Player(0,15)
-	right = [
-	    SpriteStripAnim('sprites/rightwalk.png', ((0,0,64,64)), 9, 0, True, frames),
-	    SpriteStripAnim('sprites/rightwalk.png', ((64,0,64,64)), 0, 0, True, frames),
-	    SpriteStripAnim('sprites/rightwalk.png', ((128,0,64,64)), 0, 0, True, frames),
-	    SpriteStripAnim('sprites/rightwalk.png', ((192,64,48,64)), 0, 0, True, frames),
-	    SpriteStripAnim('sprites/rightwalk.png', ((256,0,64,64)), 0, 0, True, frames),
-	    SpriteStripAnim('sprites/rightwalk.png', ((320,0,64,64)), 0, 0, True, frames),
-	    SpriteStripAnim('sprites/rightwalk.png', ((384,0,64,64)), 0, 0, True, frames),
-	    SpriteStripAnim('sprites/rightwalk.png', ((448,0,64,64)), 0, 0, True, frames),
-	    SpriteStripAnim('sprites/rightwalk.png', ((512,0,64,64)), 0, 0, True, frames),
-	] 
+	right = player.rightSprite()
+	left = player.leftSprite()
+	up = player.upSprite()
+	down = player.downSprite()
 	n = 0
 	while 1:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT: sys.exit()
 
-		playerImage = playerMove(player,n,right, defaultPlayer)
+		playerImage = player.playerMove(player, n, right, left, up, down, defaultPlayer)
 		screen.blit(background,(0,0))
 		screen.blit(playerImage,(player.x, player.y))
 		pygame.display.flip()
 
-def playerMove(player, n, spritesheet, defaultPlayer):
-	keys = pygame.key.get_pressed()
-	if keys[K_d]:
-	    if n >= len(spritesheet):
-	        n = 0
-	    image = spritesheet[n].next()
-	    player.x +=2
-	    return image
-	else: 
-		return defaultPlayer
 
 
 
